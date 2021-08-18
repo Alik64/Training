caf3d9624ac9be6e2965e8e822b58d63534b06e5
Albert <albert.barsamov@gmail.com>
2021-08-18 20:55:14 +0200

    Correction epreuve rectangle

--- Epreuve_du_feu/rectangle/while.js
+++ Epreuve_du_feu/rectangle/while.js
@@ -10,51 +10,54 @@ $> cat c1.txt           $> cat c2.txt
                         883700 */
 
 var fs = require('fs')
-const petit = fs.readFileSync('c1.txt', 'utf-8').split('\n')//[ '123', '321', '123' ]
-const grand = fs.readFileSync('c2.txt', 'utf-8').split('\n')//[ '930870', '081235', '973217', '091230', '883700' ]
-// let petit = ['123', '321', '123'];
-// let grand = ['930870', '081235', '973217', '091230', '883700']
-let posY = 0 // Position Y coin Haut-Gauche
-let posX = 0 // Position X coin Haut-Gauche
-let localiser = function () {
-    let y = 0 // position Y initiale
-    let x = 0 // position X initial
-    let n = 0 // numéro d'élément du Grand rectagle
-    while (y < Number(grand.length)) {                  //on tourne sans dépasser le Nb des items du Grand
-        while (x < Number(grand[n].length)) {        //on tourne  sans depaser taille de item
-            if (grand[y][x] === petit[y][x]             // on cherche la position de l'element haut-gauche
-            ) {
-                posY = y
-                posX = x
-                x = Number(grand[0].length)
-                y = Number(grand.length)
+let petit = fs.readFileSync('c1.txt', 'utf-8').split('\n')//[ '123', '321', '123' ]
+let grand = fs.readFileSync('c2.txt', 'utf-8').split('\n')//[ '930870', '081235', '973217', '091230', '883700' ]
+//let petit = ['123', '321', '123'];
+//let grand = ['930870', '081235', '973217', '091230', '883700']
 
-            } else {
-                x++
+
+var small = petit.map((m) => m.split("").map(m => Number(m)));
+var big = grand.map((m) => m.split("").map(m => Number(m)))
+
+// si vide - gerer
+
+
+function test(small, big, line, column) {
+    var line2 = 0;
+    while (line2 < small.length) {
+        var column2 = 0;
+        while (column2 < small[line2].length) {
+            if (small[line2][column2] != big[line + line2][column + column2]) {
+                return false
             }
+
+            column2++
         }
-        y++     // on passe au niveau superieur
-        x = 0   // on annue X pour recomencer l'opération
-        n++     // controle de longeur d'item
 
+        line2++
     }
+    return true
 }
 
-localiser()
-test()
-function test() {
-    const petit = fs.readFileSync('c1.txt', 'utf-8').split('\n')//[ '123', '321', '123' ]
-    const grand = fs.readFileSync('c2.txt', 'utf-8').split('\n')//[ '930870', '081235', '973217', '091230', '883700' ]
-    let x1 = 0
-    let y1 = 0
-    let posY1 = posY
-    let posX1 = posX
-    if (grand[posY][posX] === petit[y1][x1] && grand[posY][posX + 1] === petit[y1][x1 + 1] && grand[posY][posX + 2] === petit[y1][x1 + 2] && grand[posY + 1][posX] === petit[y1 + 1][x1] && grand[posY + 1][posX + 1] === petit[y1 + 1][x1 + 1] && grand[posY + 1][posX + 2] === petit[y1 + 1][x1 + 2] && grand[posY + 2][posX] === petit[y1 + 2][x1] && grand[posY + 2][posX + 1] === petit[y1 + 2][x1 + 1] && grand[posY + 2][posX + 2] === petit[y1 + 2][x1 + 2]
-    ) {
-        console.log("Le carré est présent")
-        console.log("Position X = " + posX + " Position Y = " + posY)
-    } else {
-        console.log("Le carré n'est pas là")
-    }
+function rectangle(small, big) {
+    var line = 0;
+    while (line < big.length) {
+        var column = 0;
+        while (column < big[line].length) {
+            if (big[line][column] == small[0][0]) {
+                if (test(small, big, line, column)) {
+                    console.log(column, line)
+                    return true
+                }
+            }
 
+            column++
+        }
+
+        line++
+    }
+    console.log("Le carré n'est pas présent")
+    return false
 }
+
+rectangle(small, big)
