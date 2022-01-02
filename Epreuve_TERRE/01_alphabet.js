f56b0d5bfa759cec13e40e9448e592dc4d606687
Albert <albert.barsamov@gmail.com>
2022-01-02 22:49:25 +0100

    Epreuve de TERRE: alphabet - ok

---
+++ Epreuve_TERRE/01_alphabet.js
@@ -0,0 +1,8 @@
+const alphabet = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+]
+let str = ""
+for (let i = 0; i < alphabet.length; i++) {
+    str += alphabet[i]
+}
+console.log(str)
